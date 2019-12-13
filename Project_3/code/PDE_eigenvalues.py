472ffae5d38735e4053164d18363e4376111ebc6
eirikngard <eirik.ngard@gmail.com>
2019-12-13 18:01:36 +0100

    trying to figure out euler

--- Project_3/code/PDE_eigenvalues.py
+++ Project_3/code/PDE_eigenvalues.py
@@ -5,131 +5,187 @@ Created on Fri Dec 13 10:54:28 2019
 @author: Eirik Nordgård
 """
 
-"""
-Using Neural Network to solve equation
-u_xx = u_t
-for a given inital condition u(x,0) = I(x) and
-boundries u(0,t) = u(L,t) = 0
-using trial funcition:
-g_trial(x,t) = (1-t)I(x) + x(1-x)t*N(x,t,P)
-N is output from neural network for input x and weights P
-"""
 import numpy as np
 from matplotlib import cm
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
+import seaborn as sns
+
+sns.set()
+sns.set_style("whitegrid")
+sns.set_palette("Set2")
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
 
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
+tf.set_random_seed(42)
+np.random.seed(42)
+
+#set to 1 for max eigenvalue, -1 for min eigenvalue
+k = -1
+
+#code for generating random, symmetric nxn matrix
+n = 6
+Q = np.random.rand(n,n)
+A = (Q.T+Q)/2
+A_temp = k*A
+A_tf = tf.convert_to_tensor(A_temp,dtype=tf.float64)
+
+#compute eigenvalues with numpy.linalg
+w_np, v_np = np.linalg.eig(A)
+idx = np.argsort(w_np)
+v_np = v_np[:,idx]
+w_min_np = np.min(w_np)
+w_max_np = np.max(w_np)
+v_min_np = v_np[:,0]
+v_max_np = v_np[:,-1]
+
+def f(x):
+    """
+    function denoted as f in the paper by YI et al.
+    x is tensor of size (n,1)
+    returns tensor of size (n,1)
+    """
+    I = tf.eye(n,dtype=tf.float64)
+    xT = tf.transpose(x)
+    term1 = tf.matmul(xT,x)*A_tf
+    term2 = (1 - tf.matmul(tf.matmul(xT,A_tf),x))*I
+    return tf.matmul((term1 + term2),x)
+
+def compute_eigval(v):
+    """
+    function for computing eigenvalue, given eigenvector v
+    v is vector of size n
+    returns a float
+    """
+    v = v.reshape(n,1)
+    vT = v.transpose()
+    num = np.matmul(np.matmul(vT,A),v)[0,0]
+    den = np.matmul(vT,v)[0,0]
+    return num/den
+
+#%%
+#setting up the NN
+prec = 0.001 #Threshold
+t_max = 8
+dt = 0.1
+Nt = int(t_max/dt)
+Nx = n
+t = np.linspace(0, (Nt-1)*dt, Nt)
+x = np.linspace(1, Nx, Nx)
+v0 = np.random.rand(n)
+
+# Create mesh and convert to tensors
+X, T = np.meshgrid(x, t)
+V, T_ = np.meshgrid(v0, t)
+
+x_ = (X.ravel()).reshape(-1, 1)
+t_ = (T.ravel()).reshape(-1, 1)
+v0_ = (V.ravel()).reshape(-1, 1)
+
+x_tf = tf.convert_to_tensor(x_,dtype=tf.float64)
+t_tf = tf.convert_to_tensor(t_,dtype=tf.float64)
+v0_tf = tf.convert_to_tensor(v0_,dtype=tf.float64)
+
+points = tf.concat([x_tf, t_tf], 1)
+
+#num_iter = 20000
+num_hidden_neurons = [10,10,10]
+num_hidden_layers = np.size(num_hidden_neurons)
+
+with tf.name_scope('dnn'):
+
+    # Input layer
+    previous_layer = points
+
+    # Hidden layers
+    for l in range(num_hidden_layers):
+        current_layer = tf.layers.dense(previous_layer, \
+                                        num_hidden_neurons[l], \
+                                        name='hidden%d'%(l+1), \
+                                        activation=tf.nn.sigmoid)
+        previous_layer = current_layer
+
+    # Output layer
+    dnn_output = tf.layers.dense(previous_layer, 1, name='output')
+
+#define loss function
+#DETTE MAA ORDNES
+#trial solution maa defineres annerledes tror AL
+with tf.name_scope('cost'):
+    trial = dnn_output*t_tf + v0_tf*k
+
+    # calculate the gradients
+    trial_dt = tf.gradients(trial, t_tf)
+
+    #reshape to allow itterating over time stemps
+    trial_rs = tf.reshape(trial,(Nt, Nx))
+    trial_dt_rs = tf.reshape(trial_dt,(Nt, Nx))
+
+    # calculate cost function, mse
+    cost_temp = 0
+    for j in range(Nt):
+        trial_temp = tf.reshape(trial_rs[j],(n,1))
+        trial_dt_temp = tf.reshape(trial_dt_rs[j],(n,1))
+        rhs = f(trial_temp) - trial_temp
+        err = tf.square(-trial_dt_temp+rhs)
+        cost_temp += tf.reduce_sum(err)
+    cost = tf.reduce_sum(cost_temp/(Nx*Nt), name='cost')
+learning_rate = 0.001
+with tf.name_scope('train'):
+    optimizer = tf.train.AdamOptimizer(learning_rate)
+    traning_op = optimizer.minimize(cost)
+
+v_dnn = None
+
+init = tf.global_variables_initializer()
 
 
-# define initial condition
-def initial(x):
-    return tf.sin(np.pi*x)
+with tf.Session() as sess:
+    # Initialize the whole graph
+    init.run()
 
-def exact(x, t):
-    return np.exp(-np.pi**2*t)*np.sin(np.pi*x)
+    # Evaluate the initial cost:
+    print('Initial cost: %g'%cost.eval())
 
-def solve(init_func, T=0.08, Nx=100, Nt=10, L=1, learning_rate=1e-3, num_iter=1e4):
-    tf.set_random_seed(4155)
-    # resetting neural network
-    tf.reset_default_graph()
-    # Defining grid interval
-    dx = L/(Nx - 1)
+    # The training of the network:
+    i = 0
+    #for i in range(num_iter):
+    while cost.eval()>prec:
+        sess.run(traning_op)
+        i += 1
+        # If one desires to see how the cost function behaves for each iteration:
+        if i % 1000 == 0:
+            print(i,'iter: %g'%cost.eval())
+    # Training is done, and we have an approximate solution to the ODE
+    print('Final cost: %g'%cost.eval())
 
-    dt = T/(Nt - 1)
+    # Store the result
+    v_dnn = tf.reshape(trial,(Nt,Nx))
+    v_dnn = v_dnn.eval()
 
-    x = np.linspace(0, L, Nx)
-    t = np.linspace(0, T, Nt)
+fig, ax = plt.subplots()
+ax.plot(t, v_dnn, color='black')
+ax.set_xlabel(r'Time $t$', fontsize=20)
+#ax.set_ylabel(r'Estimated $v_{max}$ elements', fontsize=20)
+ax.set_ylabel(r'Estimated $v_{min}$ elements', fontsize=20)
+ax.text(0.7, 0.95, 'dt = {} \n $\epsilon$ \, = {} \n i \,\, = {}'.format(dt,prec,i) , \
+        horizontalalignment='left', verticalalignment='top',\
+        transform=ax.transAxes, fontsize = 20)
+ax.tick_params(axis='both', labelsize=14)
+#plt.savefig('../figures/eigenvector_max_.pdf')
+plt.savefig('../figures/eigenvector_min_.pdf')
 
-    # Create mesh and convert to tensors
-    X, T = np.meshgrid(x, t)
+v_last_dnn = v_dnn[-1]
+w_last_dnn = compute_eigval(v_last_dnn)
+print('v0: \n', v0)
+print('v nn: \n',v_last_dnn)
+print('unit v nn: \n', v_last_dnn/np.linalg.norm(v_last_dnn))
+print('unit v max np: \n',v_max_np)
+print('unit v min np: \n',v_min_np)
+print('w nn: \n',w_last_dnn)
+print('w max numpy: \n',w_max_np)
+print('w min numpy: \n',w_min_np)
 
-    x_ = (X.ravel()).reshape(-1, 1)
-    t_ = (T.ravel()).reshape(-1, 1)
-
-    x_tf = tf.convert_to_tensor(x_)
-    t_tf = tf.convert_to_tensor(t_)
-
-    points = tf.concat([x_tf, t_tf], 1)
-
-    # SET UP NEURAL NETWORK
-
-    num_hidden_neurons = [30,30]
-    num_hidden_layers = np.size(num_hidden_neurons)
-
-    with tf.variable_scope('nn', reuse=tf.AUTO_REUSE):
-        # input layer
-        previous_layer = points
-
-        # hidden layers
-        for l in range(num_hidden_layers):
-            current_layer = tf.layers.dense(previous_layer,
-                                            num_hidden_neurons[l],
-                                            activation=tf.nn.sigmoid)
-            previous_layer = current_layer
-
-        # output layer
-        nn_output = tf.layers.dense(previous_layer, 1)
-
-    # set up cost function (error^2)
-
-    with tf.name_scope('cost'):
-        # define trial funcition
-        trial = (1-t_tf)*initial(x_tf) + x_tf*(1-x_tf)*t_tf*nn_output
-
-        # calculate the gradients
-        trial_dt = tf.gradients(trial, t_tf)
-        trial_d2x = tf.gradients(tf.gradients(trial, x_tf), x_tf)
-
-        # calculate cost function
-        err = tf.square(trial_dt[0] - trial_d2x[0])
-        cost = tf.reduce_sum(err, name='cost')
-
-    # define learning rate and minimization of cost function
-    with tf.name_scope('train'):
-        optimizer = tf.train.AdamOptimizer(learning_rate)
-        training_op = optimizer.minimize(cost)
-
-    # definie itialization of all nodes
-    init = tf.global_variables_initializer()
-
-    # define a storage value for the solution
-    u_nn = None
-
-    # CALCULATE AND SOLVE THE PDE
-    with tf.Session() as session:
-        # Initialize the computational graph
-        init.run()
-
-        #print('Initial cost: %g'%cost.eval())
-
-        for i in range(int(num_iter)):
-            session.run(training_op)
-
-        #print('Final cost: %g'%cost.eval())
-
-        u_nn = trial.eval()
-
-    # define exact solution
-
-    # reshape arrays
-    U_nn = u_nn.reshape((Nt, Nx))
-    return U_nn, x, t
-
-
-if __name__ == "__main__":
-    u, x, t = solve(initial, T=0.02)
-
-
-
-    print(f"MSE = {np.mean((u[-1, :]-exact(x, t[-1]))**2)}")
-
-    plt.figure(1)
-    plt.plot(x, u[-1, :], label=f"Neural Network, t = {t[-1]}")
-    plt.plot(x, exact(x, t[-1]), label=f"u_e, t = {t[-1]}")
-    plt.plot(x, u[5, :], label=f"Neural Network, t = {t[5]}")
-    plt.plot(x, exact(x, t[5]), label=f"u_e, t = {t[5]}")
-    plt.legend()
-    #plt.savefig("../figures/NN_solved.pdf")
-    plt.show()
+plt.show()
--- Project_3/code/PDE_euler.py
+++ Project_3/code/PDE_euler.py
@@ -40,63 +40,58 @@ def exact(x, t):
 def initial(x):
     return np.sin(np.pi*x) 
 #%%
-u1, x1 = solve(initial,0.01,Nt=10)
-u2, x2 = solve(initial,0.1,Nt=10)
-u3, x3 = solve(initial,0.4,Nt=10)
-u4, x4 = solve(initial,0.8,Nt=10)
-u1_e = exact(x1, 0.01)
-u2_e = exact(x2, 0.1)
-u3_e = exact(x3, 0.4)
-u4_e = exact(x4, 0.8)
-
+#Solving the equation for two dx
+"""
+u1, x1 = solve(initial,0.5,Nt=100) #Gived dx = 0.1
+u1_e = exact(x1,0.5)
 in1 = initial(x1)
-
-u5, x5 = solve(initial,0.1,Nt=100)
-u5_e = exact(x5, 0.1)
-in5 = initial(x5)
-
+u2, x2 = solve(initial,0.005,Nt=100) #Gived dx = 0.01
+u2_e = exact(x2,0.005)
 in2 = initial(x2)
+"""
+u1, x1 = solve(initial,0.1,Nt=19) #Gived dx = 0.1
+u1_e = exact(x1,0.1)
+in1 = initial(x1)
+u2, x2 = solve(initial,0.1,Nt=1300) #Gived dx = 0.01
+u2_e = exact(x2,0.1)
+in2 = initial(x2)
+u3, x3 = solve(initial,0.6,Nt=125) #Gived dx = 0.1
+u3_e = exact(x3,0.6)
+in3 = initial(x3)
+u4, x4 = solve(initial,0.6,Nt=10000) #Gived dx = 0.01
+u4_e = exact(x4,0.6)
+in4 = initial(x4)
+#%%
+T=0.6
+L=1
+Nt=10000
+
+dt = T/(Nt - 1)
+dx = np.sqrt(2*dt/0.95) #To ensure stability
+Nx = int(L/dx) + 1
 
 #%%
 plt.figure()
 plt.subplot(2,1,1)
-plt.title("Euler solutions after $\Delta t=0.001$",fontsize=35)
-plt.plot(u5[20],linewidth=6)
-plt.plot(u5[40],linewidth=6)
-plt.plot(u5[60],linewidth=6)
-plt.plot(u5_e,linewidth=6,label="Exact")
-plt.plot(in5,linewidth=6,label="Initial")
+plt.xlabel("Euler solutions after $\Delta x=0.1$, t=0.1",fontsize=35)
+plt.plot(u1[1,:],linewidth=4, label="u, t=0.1 dx=0.1")
+plt.plot(u3[1,:],linewidth=4, label="u, t=0.6 dx=0.1")
+plt.plot(u1_e,linewidth=4,label="Exact")
+plt.plot(in1,linewidth=4,label="Initial")
 plt.tick_params(axis='both', which='major', labelsize=30)
-plt.legend(fontsize=25)
+plt.legend(fontsize=15)
+
 plt.subplot(2,1,2)
-plt.plot(u2[2],linewidth=6)
-plt.plot(u2[4],linewidth=6)
-plt.plot(u2[6],linewidth=6)
-plt.plot(u2_e,linewidth=6,label="Exact")
-plt.plot(in2,linewidth=6,label="Initial")
+plt.xlabel("Euler solutions after $\Delta x=0.01$, t=0.1",fontsize=35)
+plt.plot(u2[90,:],linewidth=4, label="u, t=0.1 dx=0.01")
+plt.plot(u4[80,:],linewidth=4, label="u, t=0.6 dx=0.1")
+plt.plot(u2_e,linewidth=4,label="Exact")
+plt.plot(in2,linewidth=4,label="Initial")
 plt.tick_params(axis='both', which='major', labelsize=30)
-plt.legend(fontsize=25)
-plt.savefig('figures/euler_dt0.001.png')
+plt.legend(fontsize=15)
+#plt.savefig('figures/euler_dt0.001.png')
 plt.show()
-#%%
-plt.figure()
-plt.subplot(2, 2, 1)
-plt.plot(u1[1])
-plt.plot(u1[3])
-plt.plot(u1[6])
-plt.subplot(2, 2, 2)
-plt.plot(u2[1])
-plt.plot(u2[3])
-plt.plot(u2[6])
-plt.subplot(2, 2, 3)
-plt.plot(u3[1])
-plt.plot(u3[3])
-plt.plot(u3[6])
-plt.subplot(2, 2, 4)
-plt.plot(u4[1])
-plt.plot(u4[3])
-plt.plot(u4[6])
-plt.show()
+
 #%%
 U=[]
 xx=[]
@@ -131,16 +126,18 @@ print(f'MSE NN is {mse_1:.5f}')
 print(f'MSE NN is {mse_3:.5f}')
 
 #%%
-"""
+
 # plt.figure(2)
 # plt.plot(x2, initial(x2), label="inital")
+plt.figure()
 plt.plot(x2, u1_2[-1, :], label="u, t=0.3, $\Delta x=0.1$")
 plt.plot(x4, u2_2[-1, :], label="u, t=0.3, $\Delta x=0.01$")
 plt.plot(x2, exact(x2, 0.3), '--', label="$u_e$")
 plt.legend()
 #plt.savefig("../figures/FD_solved.pdf")
 plt.show()
-
+#%%
+"""
     # compute MSE of the error for the different cases:
     print("---------For t = 0.02:---------")
     print(f"dx = 0.1  | MSE = {np.mean((u1_1[-1, :]-exact(x1,0.02))**2)}")
